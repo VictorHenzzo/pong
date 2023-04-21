791b7796b72eb68aa75f5ac38be9c8e1c38dc42f
Victor H <victorhenzzowp@gmail.com>
2023-04-21 16:41:37 -0300

    Performace improve

--- lib/game/pong.dart
+++ lib/game/pong.dart
@@ -1,3 +1,10 @@
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
-class Pong extends FlameGame {}
+class Pong extends FlameGame with SingleGameInstance {
+  @override
+  Color backgroundColor() => const Color(0x00000000);
+
+  @override
+  Future<void> onLoad() async {}
+}
